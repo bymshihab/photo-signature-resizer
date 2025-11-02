9259c87c94ce170f1af5812cfefb8cdf02354b19
bymshihab <khaledshihab221@gmail.com>
2025-11-02 16:01:03 +0600

    facebook environment redirection problem solved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { ImageResizerComponent } from "./image-resizer/image-resizer.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ ImageResizerComponent],
+  imports: [ImageResizerComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'photo-signature-app';
+
+  ngOnInit() {
+    if (this.isFacebookApp()) {
+      // Redirect to open in default browser
+      window.location.href = 'https://your-app-url.com';
+      alert('Please open this app in a browser like Chrome or Safari for full functionality.');
+    }
+  }
+
+  isFacebookApp(): boolean {
+    const ua = navigator.userAgent || navigator.vendor || (window as any)['opera'];
+    return (ua.indexOf("FBAN") > -1) || (ua.indexOf("FBAV") > -1);
+  }
 }
